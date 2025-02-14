2939999920046f368066951df950bbb4f655cb76
Vishakha Pandole <vishakha6074@gmail.com>
2025-02-14 09:38:26 +0530

    Day3

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request ,jsonify
+from test   import TextToNum
+import pickle
 app=Flask(__name__)
 
 @app.route("/")
@@ -10,6 +12,23 @@ def predict():
     if request.method == "POST":
         msg = request.form.get("message")
         print(msg)
+
+        cl=TextToNum(msg)
+        cl.cleaner()
+        cl.token()
+        cl.removeStop()
+        st=cl.stemme()
+        stvc=''.join(st)
+        with open("cv.pkl","rb") as vc_file:
+            cv=pickle.load(vc_file)
+        dt=cv.transform([stvc]).toarray()
+
+        with open("stock_model.pkl","rb") as mb_file:
+            stock_model=pickle.load(mb_file)
+        pred=stock_model.predict(dt)
+        print(pred)
+        return render_template("result.html", prediction=pred[0]) 
+
     else:
         return  render_template("predict.html")
 
